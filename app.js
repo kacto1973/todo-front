ede6117daf070009613ebc02683db2f5e015b284
kacto1973 <a222212359@unison.mx>
2026-03-03 11:40:36 -0700

    fix de api url

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/tasks";
+const API_URL = "https://todo-back-dsfv.onrender.com/tasks";
 
 const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
